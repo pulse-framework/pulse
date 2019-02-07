ad06c7b9c4b29852f7a5f3140d60f67456191d50
Jamie Pine <ijamespine@me.com>
2019-02-07 14:01:54 +0000

    x

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-import pulse from "./lib";
+import Pulse from "./lib";
+import VueMixin from "./lib/Vue";
 
-export default pulse;
+export default {
+  pulse: null,
+  // Vue.use() will call this install function
+  install(Vue, config) {
+    this.pulse = new Pulse(config);
+    VueMixin(Vue, this.pulse);
+  }
+};
