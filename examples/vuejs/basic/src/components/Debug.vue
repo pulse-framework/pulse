6d5af6bdb5fef7f3b279ba0892987ba7f06efac1
Jamie Pine <ijamespine@me.com>
2019-02-15 22:17:19 +0000

    index regeneration functional

--- examples/vuejs/basic/src/components/Debug.vue
+++ examples/vuejs/basic/src/components/Debug.vue
@@ -110,7 +110,7 @@ export default {
       console.log("...");
       console.log(this);
       // this.$pulse.posts.data.liveStreamPost = true;
-      this.$posts.liveStreamPost = [];
+      this.$channels.channelOpened = false;
     }, 3000);
     // this.get("jamie");
     // this.get("casey");
