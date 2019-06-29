362551b17f6f8cf11907e0f05a0fad72438df491
James Pine <jamie@jamie-mbp.local>
2019-06-29 22:36:42 +0100

    update to vuepress config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ module.exports = {
   description: 'Application logic library for reactive Javascript frameworks',
   dest: 'dist',
   serviceWorker: true,
+  base: '/pulse/',
   themeConfig: {
     logo: '/logo.svg',
     nav: [
