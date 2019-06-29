0a3e270aec833a3d5610ccdf645d4dc11e19d113
James Pine <jamie@jamie-mbp.local>
2019-06-29 22:41:21 +0100

    gh pages

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:jamiepine/jamiepine.github.io.git master
+# git push -f git@github.com:jamiepine/jamiepine.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:jamiepine/pulse.git master:gh-pages
 
 cd -
