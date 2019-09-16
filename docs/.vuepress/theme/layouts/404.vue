3ecb0555bfc5d8b04c57ca0d6ca63b3401e2eda3
Stephen F <snipeydev@gmail.com>
2019-09-16 15:37:56 -0700

    Fixed a typo
    Had <Nav> Still in 404 page

--- docs/.vuepress/theme/layouts/404.vue
+++ docs/.vuepress/theme/layouts/404.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <Nav style="background: linear-gradient(45deg, #064789 0%, #427aa1 100%)"/>
+        <Header style="background: linear-gradient(45deg, #064789 0%, #427aa1 100%)"/>
         <div class="container mx-auto my-10 py-10 px-20 rounded border shadow-xl text-center" style="width: 100%">
             <span class="text-6xl">404</span>
             <blockquote class="mb-10">{{ getMsg() }}</blockquote>
@@ -18,7 +18,7 @@ const msgs = [
   `Looks like we've got some broken links.`
 ];
 
-import Header from '@theme/components/header';
+import Header from '@theme/components/Header';
 import Footer from '@theme/components/Footer';
 export default {
   components: { 
