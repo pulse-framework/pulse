6373dd4fcf80fbfe211f9fc17659ba0e8f3fab27
twigeee <twigetroy@gmail.com>
2019-09-25 14:39:45 +1000

    FOOTER FIX
    yeehaww

--- docs/.vuepress/theme/layouts/Changelog.vue
+++ docs/.vuepress/theme/layouts/Changelog.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="wrapper">
         <Header style="background: linear-gradient(45deg, #064789 0%, #427aa1 100%)"/>
-        <div class="font-sans leading-loose text-grey-900">
+        <div id="content" class="font-sans leading-loose text-grey-900">
             <div class="ml-16 pr-20 pb-16 pt-16">
                 <!-- Main Content -->
                 <!-- <Content /> -->
                 <ChangelogIndex />
             </div>
         </div>
-        <Footer />
+    <Footer />
     </div>
 </template>
 
 <style>
 @import url('https://fonts.googleapis.com/css?family=Noto+Sans&display=swap');
 
-body, html, #app {
+body, html {
   height: 100%; /* makes the footer sticked to bottom */
   font-family: 'Noto Sans', sans-serif;
 }
