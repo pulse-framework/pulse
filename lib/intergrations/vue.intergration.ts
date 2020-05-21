5412e4f764269fd861b2d3aa2c867f66b3829964
Philippe <hello@philippec.me>
2020-05-21 03:50:25 -0400

    Update vue.intergration.ts

--- lib/intergrations/vue.intergration.ts
+++ lib/intergrations/vue.intergration.ts
@@ -1,47 +1,87 @@
-import Pulse from '../';
+import Pulse from '..';
+import State from '../state';
+import {ComponentContainer, SubscriptionContainer} from '../sub';
+import {normalizeDeps, getInstance} from '../utils';
+import Group from '../collection/group';
+
+type keyedState = {
+	[key: string]: State
+};
 
 export default {
-  name: 'vue',
-  bind(pulseConstructor) {
-    pulseConstructor.install = Vue => {
-      const pulse = globalThis.__pulse;
-      const global = pulse._private.global;
-      const config = pulse._private.global.config;
-      Vue.mixin({
-        beforeCreate() {
-          // bind root properties
-          Object.keys(global.contextRef).forEach(moduleInstance => {
-            this['$' + moduleInstance] = global.contextRef[moduleInstance];
-          });
-          if (pulse.utils) this.$utils = pulse.utils;
-          if (pulse.services) this.$services = pulse.services;
+	name: 'vue',
+	bind(pulseConstructor) {
+		pulseConstructor.install = (Vue, options) => {
+			const pulse = globalThis.__pulse;
+			Vue.mixin({
+				beforeCreate() {
+					// bind root properties
+					if (pulseConstructor.State) this.State = pulseConstructor.State;
+					if (pulseConstructor.Collection) this.Collection = pulseConstructor.Collection;
+					if (pulseConstructor.API) this.API = pulseConstructor.API;
+					if (pulseConstructor.Computed) this.Computed = pulseConstructor.Computed;
+					if (usePulse) this.usePulse.bind(usePulse);
 
-          // register component with Pulse
-          global.subs.registerComponent(this);
+					// Object.keys(global.contextRef).forEach(moduleInstance => {
+					// 	this['$' + moduleInstance] = global.contextRef[moduleInstance];
+					// });
+					// if (pulse.utils) this.$utils = pulse.utils;
+					// if (pulse.services) this.$services = pulse.services;
 
-          // alias map
-          const mapData = global.subs.mapData.bind(global.subs);
+					// // register component with Pulse
+					// global.subs.registerComponent(this);
 
-          this.mapData = properties => mapData(properties, this);
-        },
-        mounted() {
-          if (this.__pulseUniqueIdentifier && config.waitForMount)
-            pulse.mount(this);
-        },
-        beforeDestroy() {
-          if (this.__pulseUniqueIdentifier && config.autoUnmount)
-            global.subs.unmount(this);
-        }
-      });
-    };
-  },
-  updateMethod(componentInstance: any, updatedData: Object) {
-    for (let dataKey in updatedData) {
-      componentInstance.$set(componentInstance, dataKey, updatedData[dataKey]);
-    }
-  },
-  onReady(pulseConstructor: Pulse) {
-    const Vue = pulseConstructor.intergration.frameworkConstructor;
-    Vue.use(pulseConstructor);
-  }
+					// // alias map
+					// const mapData = global.subs.mapData.bind(global.subs);
+
+					// this.mapData = properties => mapData(properties, this);
+				},
+				mounted() {
+					// if (this.__pulseUniqueIdentifier && config.waitForMount)
+					// 	pulse.mount(this);
+				},
+				beforeDestroy() {
+					// if (this.__pulseUniqueIdentifier && config.autoUnmount)
+					// 	global.subs.unmount(this);
+				}
+			});
+		};
+	},
+	updateMethod(componentInstance: any, updatedData: Object) {
+		for (let dataKey in updatedData) {
+			componentInstance.$set(componentInstance, dataKey, updatedData[dataKey]);
+		}
+	},
+	onReady(pulseInstance: any | Pulse) {
+		const Vue = pulseInstance.intergration.frameworkConstructor;
+		pulseInstance.usePulse = (deps: Array<State> | State) => usePulse(deps, pulseInstance);
+		Vue.use(pulseInstance);
+	}
 };
+
+export function usePulse(deps: Array<State | keyedState> | State, pulseInstance?: Pulse) {
+	let depsArray = normalizeDeps(deps as Array<State>);
+	if (!pulseInstance) pulseInstance = getInstance(depsArray[0]);
+
+	// The final list of states and dependancies 
+	let depsArrayFinal: Array<State> = [];
+
+	// this allows you to pass in a keyed object of States and subscribe to all  State within the first level of the object. Useful if you wish to subscribe a component to several State instances at the same time.
+	depsArray.forEach(dep => {
+		if (dep instanceof State) depsArrayFinal.push(dep);
+		else if (typeof dep === 'object')
+			for (let d in dep as keyedState) {
+				if ((dep[d] as any) instanceof State) depsArrayFinal.push(dep[d]);
+			}
+	});
+
+	// get Vue constructor
+	const Vue = pulseInstance.intergration.frameworkConstructor;
+	if (!Vue) return;
+
+	// return depsArray.map(dep => {
+	// 	if (dep instanceof State) return dep.getPublicValue();
+	// 	return dep;
+	// });
+	return depsArrayFinal;
+}
