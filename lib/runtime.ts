0b5125744988baea8873175b6c78a0f20cf2e624
James Pine <jamie@notify.me>
2020-06-24 20:16:20 -0700

    moved computed

--- lib/runtime.ts
+++ lib/runtime.ts
@@ -4,137 +4,139 @@ import { CallbackContainer, ComponentContainer, SubscriptionContainer } from './
 import Group from './collection/group';
 
 export interface Job {
-	state: State;
-	newState?: any;
+  state: State;
+  newState?: any;
 }
 export default class Runtime {
-	private current: Job = null;
-	private queue: Array<Job> = [];
-	private complete: Array<Job> = [];
-	private tasksOnceComplete: Array<() => any> = [];
+  private current: Job = null;
+  private queue: Array<Job> = [];
+  private complete: Array<Job> = [];
+  private tasksOnceComplete: Array<() => any> = [];
 
-	public trackState: boolean = false;
-	public foundState: Set<State> = new Set();
+  public trackState: boolean = false;
+  public foundState: Set<State> = new Set();
 
-	constructor(private instance: () => Pulse) { }
+  constructor(private instance: () => Pulse) {}
 
-	public ingest(state: State, newState?: any, perform: boolean = true): void {
-		let job: Job = { state, newState };
-		// grab nextState if newState not passed
-		if (arguments[1] === undefined) job.newState = job.state.nextState;
+  public ingest(state: State, newState?: any, perform: boolean = true): void {
+    let job: Job = { state, newState };
+    // grab nextState if newState not passed, compute if needed
+    if (arguments[1] === undefined) {
+      job.newState = job.state.computeValue
+        ? job.state.computeValue(job.state.nextState)
+        : job.state.nextState;
+    }
 
-		this.queue.push(job);
+    this.queue.push(job);
 
-		// if no current job, begin the next!
-		if (perform) this.perform(this.queue.shift());
-	}
+    // if no current job, begin the next!
+    if (perform) this.perform(this.queue.shift());
+  }
 
-	private perform(job: Job): void {
-		// debugger;
-		this.current = job;
-		job.state.previousState = copy(job.state._masterValue);
+  private perform(job: Job): void {
+    // debugger;
+    this.current = job;
+    job.state.previousState = copy(job.state._masterValue);
 
-		// write new value as result of mutation
-		job.state.privateWrite(job.newState);
+    // write new value as result of mutation
+    job.state.privateWrite(job.newState);
 
-		// perform side effects
-		this.sideEffects(job.state);
+    // perform side effects
+    this.sideEffects(job.state);
 
-		// declare completed
-		this.complete.push(job);
+    // declare completed
+    this.complete.push(job);
 
-		this.current = null;
+    this.current = null;
 
-		if (this.instance().config.logJobs) console.log(`Completed Job: Name:${job.state.name}`, job);
+    if (this.instance().config.logJobs) console.log(`Completed Job: Name:${job.state.name}`, job);
 
-		// continue the loop and perform the next job or update subscribers
-		if (this.queue.length > 0) this.perform(this.queue.shift());
-		else {
-			setTimeout(() => {
-				this.updateSubscribers();
-			});
-		}
-	}
+    // continue the loop and perform the next job or update subscribers
+    if (this.queue.length > 0) this.perform(this.queue.shift());
+    else {
+      setTimeout(() => {
+        this.updateSubscribers();
+      });
+    }
+  }
 
-	private sideEffects(state: State) {
-		let dep = state.dep;
+  private sideEffects(state: State) {
+    let dep = state.dep;
 
-		// cleanup dynamic deps
-		dep.dynamic.forEach(state => {
-			state.dep.deps.delete(dep);
-		});
-		dep.dynamic = new Set();
+    // cleanup dynamic deps
+    dep.dynamic.forEach(state => {
+      state.dep.deps.delete(dep);
+    });
+    dep.dynamic = new Set();
 
-		// this should not be used on root state class as it would be overwritten by extentions
-		// this is used mainly to cause group to generate its output after changing
-		if (typeof state.sideEffects === 'function') state.sideEffects();
+    // this should not be used on root state class as it would be overwritten by extentions
+    // this is used mainly to cause group to generate its output after changing
+    if (typeof state.sideEffects === 'function') state.sideEffects();
 
-		for (let watcher in state.watchers) {
-			if (typeof state.watchers[watcher] === 'function')
-				state.watchers[watcher](state.getPublicValue());
-		}
+    for (let watcher in state.watchers) {
+      if (typeof state.watchers[watcher] === 'function')
+        state.watchers[watcher](state.getPublicValue());
+    }
 
-		// ingest dependents
-		dep.deps.forEach(state => {
-			this.ingest(state, state.computeValue ? state.computeValue() : state.nextState, false);
-		});
-	}
+    // ingest dependents
+    dep.deps.forEach(state => this.ingest(state, undefined, false));
+  }
 
-	private updateSubscribers(): void {
-		let componentsToUpdate: Set<SubscriptionContainer> = new Set();
-		this.complete.forEach(job =>
-			job.state.dep.subs.forEach(cC => {
-				// for containers that require props to be passed
-				if (cC.passProps) {
-					let localKey: string;
-					// find the local key for this update by comparing the State instance from this job to the state instances in the mappedStates object
-					for (let key in cC.mappedStates) if (cC.mappedStates[key] === job.state) localKey = key;
-					// once a matching key is found push it into the SubscriptionContainer
-					if (localKey) cC.keysChanged.push(localKey);
-				}
-				componentsToUpdate.add(cC);
-			})
-		);
+  private updateSubscribers(): void {
+    let componentsToUpdate: Set<SubscriptionContainer> = new Set();
+    this.complete.forEach(job =>
+      job.state.dep.subs.forEach(cC => {
+        // for containers that require props to be passed
+        if (cC.passProps) {
+          let localKey: string;
+          // find the local key for this update by comparing the State instance from this job to the state instances in the mappedStates object
+          for (let key in cC.mappedStates) if (cC.mappedStates[key] === job.state) localKey = key;
+          // once a matching key is found push it into the SubscriptionContainer
+          if (localKey) cC.keysChanged.push(localKey);
+        }
+        componentsToUpdate.add(cC);
+      })
+    );
 
-		// perform component or callback updates
-		componentsToUpdate.forEach(cC => {
-			// are we dealing with a CallbackContainer?
-			if (cC instanceof CallbackContainer) {
-				// just invoke the callback
-				(cC as CallbackContainer).callback();
-				// is this a ComponentContainer
-			} else if (cC instanceof ComponentContainer) {
-				// call the current intergration's update method
-				this.instance().intergration.updateMethod(cC.instance, Runtime.assembleUpdatedValues(cC));
-			}
-		});
+    // perform component or callback updates
+    componentsToUpdate.forEach(cC => {
+      // are we dealing with a CallbackContainer?
+      if (cC instanceof CallbackContainer) {
+        // just invoke the callback
+        (cC as CallbackContainer).callback();
+        // is this a ComponentContainer
+      } else if (cC instanceof ComponentContainer) {
+        // call the current intergration's update method
+        this.instance().intergration.updateMethod(cC.instance, Runtime.assembleUpdatedValues(cC));
+      }
+    });
 
-		if (this.instance().config.logJobs && componentsToUpdate.size > 0)
-			console.log(`Rendered Components`, componentsToUpdate);
+    if (this.instance().config.logJobs && componentsToUpdate.size > 0)
+      console.log(`Rendered Components`, componentsToUpdate);
 
-		this.complete = [];
-		// run any tasks for next runtime
-		this.tasksOnceComplete.forEach(task => typeof task === 'function' && task());
-		this.tasksOnceComplete = [];
-	}
+    this.complete = [];
+    // run any tasks for next runtime
+    this.tasksOnceComplete.forEach(task => typeof task === 'function' && task());
+    this.tasksOnceComplete = [];
+  }
 
-	public getFoundState() {
-		this.trackState = false;
-		const ret = this.foundState;
-		this.foundState = new Set();
-		return ret;
-	}
+  public getFoundState() {
+    this.trackState = false;
+    const ret = this.foundState;
+    this.foundState = new Set();
+    return ret;
+  }
 
-	public nextPulse(callback: () => any) {
-		this.tasksOnceComplete.push(callback);
-	}
+  public nextPulse(callback: () => any) {
+    this.tasksOnceComplete.push(callback);
+  }
 
-	static assembleUpdatedValues(cC: SubscriptionContainer) {
-		let returnObj: any = {};
-		cC.keysChanged.forEach(changedKey => {
-			// extract the value from State for changed keys
-			returnObj[changedKey] = cC.mappedStates[changedKey].value;
-		});
-		return returnObj;
-	}
+  static assembleUpdatedValues(cC: SubscriptionContainer) {
+    let returnObj: any = {};
+    cC.keysChanged.forEach(changedKey => {
+      // extract the value from State for changed keys
+      returnObj[changedKey] = cC.mappedStates[changedKey].value;
+    });
+    return returnObj;
+  }
 }
--- lib/state.ts
+++ lib/state.ts
@@ -4,241 +4,241 @@ import { copy, shallowmerge } from './utils';
 import { deepmerge } from './helpers/deepmerge';
 
 export class State<ValueType = any> {
-	public _masterValue: ValueType = null;
-	public set value(val: ValueType) {
-		this._masterValue = val;
-	}
-	public get value(): ValueType {
-		if (this.instance().runtime.trackState) this.instance().runtime.foundState.add(this);
-		return this._masterValue;
-	}
-	public output?: any;
-	public watchers: { [key: string]: any } = {};
-	public previousState: ValueType = null;
-	public dep: Dep = null;
-	public nextState: ValueType = null;
-	public isSet: boolean = false; // has been changed from inital value
-	public persistState: boolean = false;
-	public name?: string;
-	public valueType?: string;
-	// sideEffects can be set by extended classes, such as Groups to build their output.
-	public sideEffects?: Function;
-	// computeValue is the method to return a new value. it is undefined in State but can be used by extended classes such as Computed, which creates it's own value
-	public computeValue?: () => any;
+  public _masterValue: ValueType = null;
+  public set value(val: ValueType) {
+    this._masterValue = val;
+  }
+  public get value(): ValueType {
+    if (this.instance().runtime.trackState) this.instance().runtime.foundState.add(this);
+    return this._masterValue;
+  }
+  public output?: any;
+  public watchers: { [key: string]: any } = {};
+  public previousState: ValueType = null;
+  public dep: Dep = null;
+  public nextState: ValueType = null;
+  public isSet: boolean = false; // has been changed from inital value
+  public persistState: boolean = false;
+  public name?: string;
+  public valueType?: string;
+  // sideEffects can be set by extended classes, such as Groups to build their output.
+  public sideEffects?: Function;
+  // computeValue is the method to return a new value. it is undefined in State but can be used by extended classes such as Computed, which creates it's own value
+  public computeValue?: (newState?: ValueType) => ValueType;
 
-	public set bind(value: ValueType) {
-		this.set(value);
-	}
-	public get bind(): ValueType {
-		return this._masterValue;
-	}
-	public get exists(): boolean {
-		return !!this.value; // is value truthey or falsey
-	}
+  public set bind(value: ValueType) {
+    this.set(value);
+  }
+  public get bind(): ValueType {
+    return this._masterValue;
+  }
+  public get exists(): boolean {
+    return !!this.value; // is value truthey or falsey
+  }
 
-	constructor(public instance: () => Pulse, public initalState, deps: Array<Dep> = []) {
-		this.dep = new Dep(deps);
-		this.privateWrite(initalState);
-		this.nextState = copy(initalState);
-	}
+  constructor(public instance: () => Pulse, public initalState, deps: Array<Dep> = []) {
+    this.dep = new Dep(deps);
+    this.privateWrite(initalState);
+    this.nextState = copy(initalState);
+  }
 
-	/**
-	 * Directly set state to a new value, if nothing is passed in State.nextState will be used as the next value
-	 * @param {Object} newState - The new value for this state
-	 */
-	public set(
-		newState?: ValueType | SetFunc<ValueType>,
-		options: { background?: boolean } = {}
-	): this {
-		// if newState not provided, just ingest update with existing value
-		if (newState === undefined) {
-			this.instance().runtime.ingest(this, undefined);
-			return this;
-		}
-		// if newState is a function, run that function and supply existing value as first param
-		if (typeof newState === 'function')
-			newState = (newState as SetFunc<ValueType>)(this._masterValue);
+  /**
+   * Directly set state to a new value, if nothing is passed in State.nextState will be used as the next value
+   * @param {Object} newState - The new value for this state
+   */
+  public set(
+    newState?: ValueType | SetFunc<ValueType>,
+    options: { background?: boolean } = {}
+  ): this {
+    // if newState not provided, just ingest update with existing value
+    if (newState === undefined) {
+      this.instance().runtime.ingest(this, undefined);
+      return this;
+    }
+    // if newState is a function, run that function and supply existing value as first param
+    if (typeof newState === 'function')
+      newState = (newState as SetFunc<ValueType>)(this._masterValue);
 
-		// check type if set and correct otherwise exit
-		if (this.valueType && !this.isCorrectType(newState)) {
-			console.warn(
-				`Pulse: Error setting state: Incorrect type (${typeof newState}) was provided. Type fixed to ${
-				this.valueType
-				}`
-			);
-			return this;
-		}
+    // check type if set and correct otherwise exit
+    if (this.valueType && !this.isCorrectType(newState)) {
+      console.warn(
+        `Pulse: Error setting state: Incorrect type (${typeof newState}) was provided. Type fixed to ${
+          this.valueType
+        }`
+      );
+      return this;
+    }
 
-		// ingest update using most basic mutation method
-		if (options.background) {
-			this.privateWrite(newState);
-			if (this.sideEffects) this.sideEffects();
-		} else {
-			this.instance().runtime.ingest(this, newState);
-		}
+    // ingest update using most basic mutation method
+    if (options.background) {
+      this.privateWrite(newState);
+      if (this.sideEffects) this.sideEffects();
+    } else {
+      this.instance().runtime.ingest(this, newState);
+    }
 
-		this.isSet = true;
-		return this;
-	}
+    this.isSet = true;
+    return this;
+  }
 
-	public getPublicValue(): ValueType {
-		if (this.output !== undefined) return this.output;
-		return this._masterValue;
-	}
+  public getPublicValue(): ValueType {
+    if (this.output !== undefined) return this.output;
+    return this._masterValue;
+  }
 
-	public patch(targetWithChange, config: { deep?: boolean } = {}): this {
-		if (!(typeof this._masterValue === 'object')) return this;
+  public patch(targetWithChange, config: { deep?: boolean } = {}): this {
+    if (!(typeof this._masterValue === 'object')) return this;
 
-		this.nextState =
-			config.deep === false
-				? shallowmerge(this.nextState, targetWithChange)
-				: deepmerge(this.nextState, targetWithChange);
+    this.nextState =
+      config.deep === false
+        ? shallowmerge(this.nextState, targetWithChange)
+        : deepmerge(this.nextState, targetWithChange);
 
-		this.set();
-		return this;
-	}
+    this.set();
+    return this;
+  }
 
-	public interval(setFunc: (currentValue: any) => any, ms?: number): this {
-		setInterval(() => {
-			this.set(setFunc(this.value));
-		}, ms || 1000);
-		return this;
-	}
+  public interval(setFunc: (currentValue: any) => any, ms?: number): this {
+    setInterval(() => {
+      this.set(setFunc(this.value));
+    }, ms || 1000);
+    return this;
+  }
 
-	public persist(key?: string): this {
-		this.persistState = true;
-		if (!key && this.name) {
-			key = this.name;
-		} else if (!key) {
-			console.warn('Pulse Persist Error: No key provided');
-		} else {
-			this.name = key;
-		}
-		const storage = this.instance().storage;
-		storage.persistedState.add(this);
-		if (storage.isPromise) {
-			storage.get(this.name).then((val: ValueType) => {
-				if (val === null) storage.set(this.name, this.value);
-				this.instance().runtime.ingest(this, val);
-			});
-		} else {
-			let value = storage.get(this.name);
-			if (value === null) storage.set(this.name, this.value);
-			else this.instance().runtime.ingest(this, value);
-		}
-		return this;
-	}
+  public persist(key?: string): this {
+    this.persistState = true;
+    if (!key && this.name) {
+      key = this.name;
+    } else if (!key) {
+      console.warn('Pulse Persist Error: No key provided');
+    } else {
+      this.name = key;
+    }
+    const storage = this.instance().storage;
+    storage.persistedState.add(this);
+    if (storage.isPromise) {
+      storage.get(this.name).then((val: ValueType) => {
+        if (val === null) storage.set(this.name, this.value);
+        this.instance().runtime.ingest(this, val);
+      });
+    } else {
+      let value = storage.get(this.name);
+      if (value === null) storage.set(this.name, this.value);
+      else this.instance().runtime.ingest(this, value);
+    }
+    return this;
+  }
 
-	// this creates a watcher that will fire a callback then destroy itself after invoking
-	public onNext(callback: (value: ValueType) => void) {
-		this.watchers['_on_next_'] = () => {
-			callback(this.getPublicValue());
-			delete this.watchers['_on_next_'];
-		};
-	}
+  // this creates a watcher that will fire a callback then destroy itself after invoking
+  public onNext(callback: (value: ValueType) => void) {
+    this.watchers['_on_next_'] = () => {
+      callback(this.getPublicValue());
+      delete this.watchers['_on_next_'];
+    };
+  }
 
-	public key(key: string): this {
-		// this.name = key;
-		return this;
-	}
+  public key(key: string): this {
+    // this.name = key;
+    return this;
+  }
 
-	public type(type: any): this {
-		const supportedConstructors = ['String', 'Boolean', 'Array', 'Object', 'Number'];
-		if (typeof type === 'function' && supportedConstructors.includes(type.name)) {
-			this.valueType = type.name.toLowerCase();
-		}
-		return this;
-	}
+  public type(type: any): this {
+    const supportedConstructors = ['String', 'Boolean', 'Array', 'Object', 'Number'];
+    if (typeof type === 'function' && supportedConstructors.includes(type.name)) {
+      this.valueType = type.name.toLowerCase();
+    }
+    return this;
+  }
 
-	public watch(key: number | string, callback: (value: any) => void): this {
-		if (typeof key !== 'string' || typeof key !== 'number' || typeof callback !== 'function') {
-			// console.error('Pulse watch, missing key or function');
-		}
-		this.watchers[key] = callback;
-		return this;
-	}
+  public watch(key: number | string, callback: (value: any) => void): this {
+    if (typeof key !== 'string' || typeof key !== 'number' || typeof callback !== 'function') {
+      // console.error('Pulse watch, missing key or function');
+    }
+    this.watchers[key] = callback;
+    return this;
+  }
 
-	public undo() {
-		this.set(this.previousState);
-	}
+  public undo() {
+    this.set(this.previousState);
+  }
 
-	public removeWatcher(key: number | string): this {
-		delete this.watchers[key];
-		return this;
-	}
+  public removeWatcher(key: number | string): this {
+    delete this.watchers[key];
+    return this;
+  }
 
-	public toggle(): this {
-		if (typeof this._masterValue === 'boolean') {
-			// @ts-ignore
-			this.set(!this._masterValue);
-		}
-		return this;
-	}
+  public toggle(): this {
+    if (typeof this._masterValue === 'boolean') {
+      // @ts-ignore
+      this.set(!this._masterValue);
+    }
+    return this;
+  }
 
-	public reset(): this {
-		reset(this);
-		return this;
-	}
+  public reset(): this {
+    reset(this);
+    return this;
+  }
 
-	// returns a fresh copy of the current value
-	public copy(): any {
-		return copy(this.value);
-	}
+  // returns a fresh copy of the current value
+  public copy(): any {
+    return copy(this.value);
+  }
 
-	public is(x: any) {
-		return this.value === x;
-	}
+  public is(x: any) {
+    return this.value === x;
+  }
 
-	public isNot(x: any) {
-		return this.value !== x;
-	}
+  public isNot(x: any) {
+    return this.value !== x;
+  }
 
-	public relate(state: State | Array<State>) {
-		if (!Array.isArray(state)) state = [state];
-		// add this to foriegn dep
-		state.forEach(state => state && state.dep.depend(this));
-		// refrence foriegn dep locally for cleanup
-		this.dep.dynamic.add(this);
-	}
+  public relate(state: State | Array<State>) {
+    if (!Array.isArray(state)) state = [state];
+    // add this to foriegn dep
+    state.forEach(state => state && state.dep.depend(this));
+    // refrence foriegn dep locally for cleanup
+    this.dep.dynamic.add(this);
+  }
 
-	// INTERNAL
-	public privateWrite(value: any) {
-		this._masterValue = copy(value);
-		this.nextState = copy(value);
+  // INTERNAL
+  public privateWrite(value: any) {
+    this._masterValue = copy(value);
+    this.nextState = copy(value);
 
-		if (this.persistState) this.instance().storage.set(this.name, value);
-	}
+    if (this.persistState) this.instance().storage.set(this.name, value);
+  }
 
-	private isCorrectType(value): boolean {
-		let type: string = typeof value;
-		if (type === 'object' && Array.isArray(value)) type = 'array';
-		return type === this.valueType;
-	}
+  private isCorrectType(value): boolean {
+    let type: string = typeof value;
+    if (type === 'object' && Array.isArray(value)) type = 'array';
+    return type === this.valueType;
+  }
 
-	public destroy(): void {
-		this.dep.deps.clear();
-		this.dep.subs.clear();
-	}
+  public destroy(): void {
+    this.dep.deps.clear();
+    this.dep.subs.clear();
+  }
 }
 
 export type StateGroupDefault = {
-	[key: string]: State | any;
+  [key: string]: State | any;
 };
 export const StateGroup = (instance: () => Pulse, stateGroup: Object): any => {
-	let group: any = {};
-	for (let name in stateGroup) {
-		group[name] = new State(instance, stateGroup[name]);
-		group[name].name = name;
-	}
-	return group;
+  let group: any = {};
+  for (let name in stateGroup) {
+    group[name] = new State(instance, stateGroup[name]);
+    group[name].name = name;
+  }
+  return group;
 };
 export default State;
 
 export function reset(instance: State) {
-	instance.isSet = false;
-	instance.previousState = null;
-	instance.privateWrite(instance.initalState);
-	if (instance.persistState) instance.instance().storage.remove(instance.name);
+  instance.isSet = false;
+  instance.previousState = null;
+  instance.privateWrite(instance.initalState);
+  if (instance.persistState) instance.instance().storage.remove(instance.name);
 }
 
 type SetFunc<ValueType> = (state: ValueType) => ValueType;
