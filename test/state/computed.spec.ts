4b1b519146550033d6932641ea7fd595ad133bbf
Haden Fletcher <haden@hadenfletcher.com>
2020-08-12 14:52:18 -0500

    test Coreful and coreless Computed functionality

--- test/state/computed.spec.ts
+++ test/state/computed.spec.ts
@@ -1,41 +1,85 @@
-import 'mocha'
-import { expect } from 'chai'
-import Pulse, { Computed, State } from '../../lib/index'
+import 'mocha';
+import { expect } from 'chai';
+import Pulse, { Computed, State } from '../../lib/index';
 
-const App = new Pulse({
+const generateState = (App: Pulse) => ({
+  string: App.State('initial value'),
+  boolean: App.State(false),
+  number: App.State(1000)
+});
+const generateComputed = (App: Pulse, state) => ({
+  STRING: App.Computed(() => state.string.value, [state.string]),
+  BOOLEAN: App.Computed(() => state.boolean.value, [state.boolean]),
+  NUMBER: App.Computed(() => state.number.value, [state.number])
+});
+
+const basicTests = (computed: { [key: string]: Computed }, state: { [key: string]: State }) => {
+  it('is both State and Computed', () => {
+    for (const stateValue of Object.values(computed)) {
+      expect(stateValue instanceof State).to.eq(true);
+      expect(stateValue instanceof Computed).to.eq(true);
+    }
+  });
+};
+
+describe('coreless Computed functionality', () => {
+  const App = new Pulse({
     noCore: true
-})
+  });
 
-const state = {
-    string: App.State('initial value'),
-    boolean: App.State(false),
-    number: App.State(1000)
-}
-const computed = {
-    STRING: App.Computed(() => state.string.value, [state.string]),
-    BOOLEAN: App.Computed(() => state.boolean.value, [state.boolean]),
-    NUMBER: App.Computed(() => state.number.value, [state.number])
-}
+  const state = generateState(App);
+  const computed = generateComputed(App, state);
 
-describe('basic Computed functionality', () => {
-    it('is both State and Computed', () => {
-        for(const stateValue of Object.values(computed)) {
-            expect(stateValue instanceof State).to.eq(true)
-            expect(stateValue instanceof Computed).to.eq(true)
-        }
-    })
+  basicTests(computed, state);
 
-    it('has correct initial value', () => {
-        expect(computed.STRING.value).to.eq('initial value')
-        expect(computed.BOOLEAN.value).to.eq(false)
-        expect(computed.NUMBER.value).to.eq(1000)
-    })
-    
-    it('recomputes automatically', () => {
-        state.string.set('changed value')
-        expect(computed.STRING.value).to.eq('changed value')
+  it('has correct initial value', () => {
+    expect(computed.STRING.value).to.eq('initial value');
+    expect(computed.BOOLEAN.value).to.eq(false);
+    expect(computed.NUMBER.value).to.eq(1000);
+  });
 
-        state.number.set(5000)
-        expect(computed.NUMBER.value).to.eq(5000)
-    })
-})
+  it('recomputes upon dependency change', () => {
+    state.string.set('changed value');
+    expect(computed.STRING.value).to.eq('changed value');
+
+    state.number.set(5000);
+    expect(computed.NUMBER.value).to.eq(5000);
+  });
+});
+
+describe('coreful Computed functionality', () => {
+  const App = new Pulse({});
+
+  const state = generateState(App);
+  const computed = generateComputed(App, state);
+
+  basicTests(computed, state);
+
+  it('has no initial value', () => {
+    expect(computed.STRING.value).to.eq(null);
+    expect(computed.BOOLEAN.value).to.eq(null);
+    expect(computed.NUMBER.value).to.eq(null);
+  });
+
+  it('computes upon Core initialization', () => {
+    const core = App.Core({});
+
+    expect(computed.STRING.value).to.eq('initial value');
+    expect(computed.BOOLEAN.value).to.eq(false);
+    expect(computed.NUMBER.value).to.eq(1000);
+  });
+
+  it('recomputes upon dependency change', () => {
+    state.string.set('changed value');
+    expect(computed.STRING.value).to.eq('changed value');
+
+    state.number.set(5000);
+    expect(computed.NUMBER.value).to.eq(5000);
+  });
+
+  // it('resets value properly', () => {
+  //   expect(computed.STRING.value).to.eq('initial value');
+  //   expect(computed.BOOLEAN.value).to.eq(false);
+  //   expect(computed.NUMBER.value).to.eq(1000);
+  // });
+});
