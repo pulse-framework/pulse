12d74764c13debcdf577064a0cc607a51e8de535
Dustin Rouillard <me@dustin.sh>
2020-09-04 22:32:23 -0700

    Fix tests for now

--- test/state/computed.spec.ts
+++ test/state/computed.spec.ts
@@ -1,6 +1,6 @@
 import 'mocha';
 import { expect } from 'chai';
-import Pulse, { Computed, State } from '../../dist';
+import Pulse, { Computed, State } from '../../lib';
 
 const generateState = (App: Pulse) => ({
   string: App.State('initial value'),
