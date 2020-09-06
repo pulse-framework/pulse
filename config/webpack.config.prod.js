87a23ff55763581fbb730af8d53e9c77ec5c3ac0
Jamie <ijamespine@me.com>
2020-09-05 19:11:24 -0700

    x

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,14 +1,27 @@
 const base = require('../webpack.config');
 const path = require('path');
 const dts = require('dts-bundle-webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   ...base('prod'),
   name: 'build',
   mode: 'production',
+  devtool: 'hidden-source-map',
   entry: {
     index: './lib/index.ts',
     next: './lib/next'
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          keep_classnames: true,
+          keep_fnames: true
+        }
+      })
+    ]
   },
   plugins: [
     new dts({
