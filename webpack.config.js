67d1bd5e11865d5204206a64b87333132daec368
Haden Fletcher <33473181+hadenpf@users.noreply.github.com>
2020-09-05 20:24:03 -0500

    building works

--- webpack.config.js
+++ webpack.config.js
@@ -7,71 +7,77 @@ const tsConfigPaths = {
   prod: path.resolve(__dirname, 'config/tsconfig.prod.json')
 };
 
-const baseConfig = {
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    library: 'pulse-framework',
-    libraryTarget: 'umd'
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts.?$/,
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              context: path.resolve(__dirname, 'config'),
-              configFile: tsConfigPaths.prod
+/**
+ * Generate a configuration object
+ * @param {'dev' | 'prod'} mode
+ */
+const getConfig = mode => {
+  const baseConfig = {
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      library: 'pulse-framework',
+      libraryTarget: 'umd'
+    },
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts.?$/,
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                context: path.resolve(__dirname, 'config'),
+                configFile: mode === 'dev' ? tsConfigPaths.dev : tsConfigPaths.prod
+              }
             }
-          }
-        ]
-      }
-    ]
-  }
+          ]
+        }
+      ]
+    }
+  };
+
+  if (mode === 'dev')
+    return {
+      ...baseConfig,
+      name: 'dev',
+      mode: 'development',
+      devtool: 'inline-source-map',
+      entry: {
+        index: './lib/index.ts',
+        next: './lib/next'
+      },
+      plugins: []
+    };
+
+  if (mode === 'prod')
+    return {
+      ...baseConfig,
+      name: 'build',
+      mode: 'production',
+      devtool: 'inline-source-map',
+      entry: {
+        index: './lib/index.ts',
+        next: './lib/next'
+      },
+      plugins: [
+        new dts({
+          name: 'pulse-framework',
+          main: 'declarations/index.d.ts',
+          out: '../dist/index.d.ts'
+        }),
+        new dts({
+          name: 'pulse-framework/next',
+          main: 'declarations/next/index.d.ts',
+          out: '../../dist/next.d.ts'
+        })
+      ]
+    };
 };
 
-// uses master ts config
-const buildConfig = {
-  ...baseConfig,
-  name: 'build',
-  mode: 'production',
-  devtool: 'inline-source-map',
-  entry: {
-    index: './lib/index.ts',
-    next: './lib/next'
-  },
-  plugins: [
-    new dts({
-      name: 'pulse-framework',
-      main: 'declarations/index.d.ts',
-      out: '../dist/index.d.ts'
-    }),
-    new dts({
-      name: 'pulse-framework/next',
-      main: 'declarations/next/index.d.ts',
-      out: '../../dist/next.d.ts'
-    })
-  ]
-};
-
-// this will use a custom tsconfig
-const devConfig = {
-  ...baseConfig,
-  name: 'dev',
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: {
-    index: './lib/index.ts',
-    next: './lib/next'
-  },
-  plugins: []
-};
-
-// tsconfig for development
-devConfig.module.rules[0].use[0].options.configFile = tsConfigPaths.dev;
+const buildConfig = getConfig('prod');
+const devConfig = getConfig('dev');
 
 module.exports = [buildConfig, devConfig];
