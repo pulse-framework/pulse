2ae4e29dd7050366b3f2e13b1bb1062e5f36b1d0
Jamie <ijamespine@me.com>
2020-10-17 14:43:13 -0700

    useWatcher fix

--- packages/pulse-react/lib/useWatcher.ts
+++ packages/pulse-react/lib/useWatcher.ts
@@ -3,8 +3,7 @@ import { State } from '@pulsejs/core';
 
 export function useWatcher<T = any>(state: State<T>, callback: (value: T) => void) {
   React.useEffect(() => {
-    const key = Math.floor(Math.random() * 1000);
-    state.watch(key, callback);
-    return () => state.removeWatcher(key);
+    const cleanup = state.watch(callback);
+    return () => state.removeWatcher(cleanup);
   }, []);
 }
