5484f427797edc456de88de35dfd70ed161ee1a0
Philippe Clesca <hello@philippec.me>
2021-09-14 12:22:37 -0400

    reinstated the index change dur to integration issues

--- packages/pulse-core/lib/index.ts
+++ packages/pulse-core/lib/index.ts
@@ -1,17 +1,3 @@
-// Types
-export { SetFunc } from './state';
-export { IJob } from './runtime';
-export { FuncType } from './action';
-export { SubscriptionContainer } from './sub';
-export { APIConfig, PulseResponse } from './api';
-export { PrimaryKey, GroupName, GroupAddOptions } from './collection/group';
-export { StorageConfig } from './storage';
-export { EventPayload, EventConfig, EventsObjFunc, EventCallbackFunc } from './event';
-export { GroupObj, DefaultDataItem, SelectorObj, Config } from './collection/collection';
-
-// Instance
-export * from './instance';
-
+export * from './internal';
 import { Pulse } from './pulse';
-
 export default Pulse;
