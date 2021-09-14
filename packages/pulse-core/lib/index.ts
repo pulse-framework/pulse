7f9ebb606def432570ab0fdb4c9cf1642689f94a
Philippe Clesca <hello@philippec.me>
2021-09-14 10:46:25 -0400

    New exported members structure

--- packages/pulse-core/lib/index.ts
+++ packages/pulse-core/lib/index.ts
@@ -1,3 +1,17 @@
-export * from './internal';
+// Types
+export { SetFunc } from './state';
+export { IJob } from './runtime';
+export { FuncType } from './action';
+export { SubscriptionContainer } from './sub';
+export { APIConfig, PulseResponse } from './api';
+export { PrimaryKey, GroupName, GroupAddOptions } from './collection/group';
+export { StorageConfig } from './storage';
+export { EventPayload, EventConfig, EventsObjFunc, EventCallbackFunc } from './event';
+export { GroupObj, DefaultDataItem, SelectorObj, Config } from './collection/collection';
+
+// Instance
+export * from './instance';
+
 import { Pulse } from './pulse';
+
 export default Pulse;
